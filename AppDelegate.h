618246c3f2f25575a6cdbe07eeda3b2043a33871
mynameisflie <preferreduser@Preferred-Users-iMac-3.local>
2013-10-31 21:44:40 -0400

    First Time I committed

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NathanTry
+//
+//  Created by Kadail on 10/31/13.
+//  Copyright (c) 2013 Adoration. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NathanTry
+//
+//  Created by Kadail on 10/31/13.
+//  Copyright (c) 2013 Adoration. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
